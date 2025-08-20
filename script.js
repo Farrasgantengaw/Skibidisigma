03eb9efdb4b2bba598d33bdabd034bd37081b1ea
Farrasgantengaw <161009921+Farrasgantengaw@users.noreply.github.com>
2025-08-20 14:41:45 +0700

    script.js

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ window.addEventListener('load', () => {
 
 const messages = [
     {
-        text: "plss mau dong",
-        image: "https://i.pinimg.com/736x/1e/7c/6f/1e7c6ff7a0c26f7eaefbce683656193d.jpg"
+        text: "me will love u a lot",
+        image: "https://i.ibb.co.com/39qjHNkj/82be04604449fefe176e7b794d17755b-1.jpg"
     },
     {
-        text: "pls klik yg kiri",
+        text: "me will protecc u",
         image: "https://i.pinimg.com/736x/90/73/a9/9073a9359bff531cd830ae7384752934.jpg"
     },
     {
-        text: "terakhir, kalo \"no\" yauda deh",
+        text: "terakhir, kalo \"no\" gaoaoa^^(gpp kalo belum siap, gomene)",
         image: "https://i.pinimg.com/736x/a6/43/f5/a643f5a40db387585b56bc767b66fddb.jpg"
     }
 ];
@@ -160,3 +160,4 @@ const handleButtonDodge = (e) => {
 noBtn.addEventListener('mouseover', handleButtonDodge);
 noBtn.addEventListener('touchstart', handleButtonDodge, { passive: false });
 noBtn.addEventListener('touchmove', (e) => e.preventDefault(), { passive: false });
+
